6c46886712a50974ded44b5d61977e44b86003ba
Giovanna Ruggio <giovannaruggio@gmail.com>
2022-02-25 07:18:57 -0500

    Create client side mutations and queries

---
+++ client/src/utils/queries.js
@@ -0,0 +1,17 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me {
+        me {
+            username
+            savedBooks {
+                bookId
+                authors
+                description
+                title
+                image
+                link
+            }
+        }
+    }
+`;
